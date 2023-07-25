9e98a461e405c91b952a3be4caabc4339cb8901e
denitiawan <deni.setiawan0510@gmail.com>
2023-07-25 16:30:15 +0700

    refactore code

--- config/SwaggerRouting.go
+++ config/SwaggerRouting.go
@@ -8,6 +8,7 @@ import (
 
 // path swagger is customable
 // path (/*any) is required for load the html page own by swagger
+// http://localhost:8899/nexsoft/doc/api/swagger/index.html
 func SwaggerRouting(router *gin.Engine) {
 	router.GET("nexsoft/doc/api/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
